b0b12a25aa7c5bb26250473b6a37647af2aed39d
SantLeht <santleht@uef.fi>
2024-02-02 06:43:00 +0200

    Modulaarisuutta parannettu luomalla luokat esteille ja raketille.Bugeja fixailtu mutta edelleen scoreboardissa pieni bugi.

--- Raketti.py
+++ Raketti.py
@@ -1,6 +1,16 @@
-import pygame.sprite
+import pygame
 
 
 class Raketti(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, image_path, initial_position):
         super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect(topleft=initial_position)
+
+    def update(self, liikuta_vasen, liikuta_oikea, screen_width):
+        self.rect.x += liikuta_vasen + liikuta_oikea
+        # Estetään raketti karkaamasta näytön ulkopuolelle
+        if self.rect.right > screen_width:
+            self.rect.right = screen_width
+        if self.rect.left < 0:
+            self.rect.left = 0
